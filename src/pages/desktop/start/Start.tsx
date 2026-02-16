6892758043e7cda091bb0800ecff36170290c1e1
Davide Iacovone <106555044+DavidIacovone@users.noreply.github.com>
2026-02-16 22:59:59 +0100

    chore(ui): update layout of the start screen on desktop

--- src/pages/desktop/start/Start.tsx
+++ src/pages/desktop/start/Start.tsx
@@ -1,9 +1,8 @@
-import { VStack, Image } from '@chakra-ui/react';
+import { VStack, Image, Button } from '@chakra-ui/react';
 import { save, open } from '@tauri-apps/plugin-dialog';
 import { useNavigate } from 'react-router-dom';
 
 import logo from '@/assets/logo.png';
-import { RoundedButton } from '@/components';
 import { useServices } from '@/hooks/useServices.tsx';
 
 export default function Start() {
@@ -35,24 +34,28 @@ export default function Start() {
   };
 
   return (
-    <VStack>
+    <VStack justifyContent='space-between' height='70%' p={4}>
       <Image src={logo} alt='logo' aspectRatio={4 / 3} width='350px' />
-      <RoundedButton
-        width='30%'
-        onClick={async () => {
-          await createNewVault();
-        }}
-      >
-        Create new nobl file
-      </RoundedButton>
-      <RoundedButton
-        width='30%'
-        onClick={async () => {
-          await loadExistingVault();
-        }}
-      >
-        Load existing nobl file
-      </RoundedButton>
+      <VStack gap={2} width='100%'>
+        <Button
+          width='30%'
+          variant='surface'
+          onClick={async () => {
+            await createNewVault();
+          }}
+        >
+          Create new nobl file
+        </Button>
+        <Button
+          width='30%'
+          variant='surface'
+          onClick={async () => {
+            await loadExistingVault();
+          }}
+        >
+          Load existing nobl file
+        </Button>
+      </VStack>
     </VStack>
   );
 }
